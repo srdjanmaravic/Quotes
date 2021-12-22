578307ec29d19136328e0b4a4cdae6f1e63b4f62
Maravic <srdjan.maravic@igt.com>
2021-12-22 09:27:10 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <Header :quoteCount="quotes.length" :maxQuotes="maxQuotes"> </Header>
+    <NewQuote @quoteAdded="addQuote"></NewQuote>
+    <QuoteGrid :quotes="quotes" @quoteDeleted="deleteQuote"> </QuoteGrid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header.vue";
+export default {
+  data() {
+    return {
+      quotes: ["Just a quote to see somting!"],
+      maxQuotes: 10,
+    };
+  },
+  methods: {
+    addQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete some quotes first!");
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(id) {
+      this.quotes.splice(id, 1);
+    },
+  },
+  components: {
+    QuoteGrid,
+    NewQuote,
+    Header,
+  },
+};
+</script>
+
+<style></style>
